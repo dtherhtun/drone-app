ea8da102e2bbd87fdad860ff295cf810d5660a4a
D Ther <dtherhtun.cw@gmail.com>
2021-06-30 00:03:22 +0630

    drone app

--- .drone.sh
+++ .drone.sh
@@ -11,8 +11,8 @@ chmod 600 ~/.ssh/known_hosts
 ssh-keyscan -H github.com > /etc/ssh/ssh_known_hosts 2> /dev/null
 
 
-git clone git@github.com:dtherhtun/drone-app.git -b deploy
-cd drone-app
+git clone git@github.com:dtherhtun/drone-app.git -b deploy app
+cd app
 sed -i "s+dther/golang-http:.*$*+dther/golang-http:${DRONE_COMMIT_SHA}+g" kustomize/bases/hola/deployment.yaml
 git add .
 git config --global user.name "drone-ci"
